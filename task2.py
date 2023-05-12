65c88d54bbf99f6ed4ce39efabcefa5465c5716a
unknown <gerasimmaks@mail.ru>
2023-05-12 18:41:47 +0300

    добавлен

---
+++ task2.py
@@ -0,0 +1,6 @@
+import datetime
+
+seconds = int(input('введите время в секундах'))
+minutes = seconds / 60
+hours = minutes / 60
+print(f'часов:{hours}  минут:{minutes}  секунд:{seconds}')
---
+++ task4.py
@@ -0,0 +1,20 @@
+revenue = int(input('введите выручку фирмы:'))
+costs = int(input('введите издержки фирмы:'))
+employee = int(input('введите численность сотрудников:'))
+
+
+if revenue > costs:
+
+    profit = revenue - costs
+    rent = profit / revenue
+    profitemp = profit / employee
+    print('выручка больше издержек')
+    print(f'прибыль: {profit}')
+    print(f'рентабельность: {rent}')
+    print(f'пприбыль фирмы на одного сотрудника: {profitemp}')
+
+else:
+    print('издержки больше выручки')
+
+
+
